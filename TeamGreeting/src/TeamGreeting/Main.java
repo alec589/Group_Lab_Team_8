9099daf9137b24755d7f437973deebff2bffdf48
alec589 <sun.zuo@northeastern.edu>
2025-10-19 09:34:55 -0400

    1

---
+++ TeamGreeting/src/TeamGreeting/Main.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package TeamGreeting;
+
+/**
+ *
+ * @author sunzuolin
+ */
+public class Main {
+     public static void main(String[] args) {
+Zuolin_Sun.main(args);
+Chunyan_Li.main(args);
+Lu_Wang.main(args);
+Kailu_Chen.main(args);
+}
+}
