83e1c6c9e90d766e24177ce534a4f0f4feb7ddeb
ChunyanLi551 <li.chunya@northeastern.edu>
2025-10-19 10:02:50 -0400

    2

---
+++ TeamGreeting/src/TeamGreeting/ChunyanLi.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package TeamGreeting;
+
+/**
+ *
+ * @author lichun
+ */
+public class ChunyanLi {
+   public static void main(String[] args) {
+System.out.println("Hello from Chunyan Li!");
+} 
+}
