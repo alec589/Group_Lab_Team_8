a618a78ac6ffea840571841ca010cf702898ee79
alec589 <sun.zuo@northeastern.edu>
2025-10-21 13:17:13 -0400

    add assignmentclass

--- Info5100-University-Example/src/info5100/university/example/Persona/Faculty/Assignment.java
+++ Info5100-University-Example/src/info5100/university/example/Persona/Faculty/Assignment.java
@@ -4,6 +4,8 @@
  */
 package info5100.university.example.Persona.Faculty;
 
+import javax.swing.ImageIcon;
+
 /**
  *
  * @author sunzuolin
@@ -12,11 +14,29 @@ public class Assignment {
     private String assignmentName;
     private int maxPoints;
     FacultyProfile facultyprofile;
+    private ImageIcon logoImage;
+    String Description;
     public Assignment(String name,FacultyProfile fp, int maxPoints) {
         this.assignmentName = name;
         this.facultyprofile =fp;
         this.maxPoints = maxPoints;
        
+    }
+
+    public ImageIcon getLogoImage() {
+        return logoImage;
+    }
+
+    public void setLogoImage(ImageIcon logoImage) {
+        this.logoImage = logoImage;
+    }
+
+    public String getDescription() {
+        return Description;
+    }
+
+    public void setDescription(String Description) {
+        this.Description = Description;
     }
 
     public FacultyProfile getFacultyprofile() {
